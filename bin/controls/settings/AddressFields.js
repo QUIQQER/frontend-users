81eba87dbfd09eaa624ab94683d455fe1f003b34
Patrick Müller <p.mueller@pcsg.de>
2017-10-20 14:00:20 +0200

    feat: Address field settings (temp commit)

--- bin/controls/settings/AddressFields.js
+++ bin/controls/settings/AddressFields.js
@@ -52,10 +52,6 @@ define('package/quiqqer/frontend-users/bin/controls/settings/AddressFields', [
             this.$Input      = this.getElm();
             this.$Input.type = 'hidden';
 
-            if (this.$Input.value.length) {
-                this.$Settings = JSON.decode(this.$Input.value);
-            }
-
             var lgPrefix = 'controls.settings.addressfields.template.';
 
             this.$Content = new Element('div', {
@@ -64,44 +60,84 @@ define('package/quiqqer/frontend-users/bin/controls/settings/AddressFields', [
                     field          : QUILocale.get(lg, lgPrefix + 'field'),
                     fieldShow      : QUILocale.get(lg, lgPrefix + 'fieldShow'),
                     fieldRequired  : QUILocale.get(lg, lgPrefix + 'fieldRequired'),
-                    labelSalutation: QUILocale.get(lg, lgPrefix + 'labelSalutation')
+                    labelSalutation: QUILocale.get('quiqqer/system', 'salutation'),
+                    labelFirstName : QUILocale.get('quiqqer/system', 'firstname'),
+                    labelLastName  : QUILocale.get('quiqqer/system', 'lastname'),
+                    labelStreet    : QUILocale.get('quiqqer/system', 'street'),
+                    labelZip       : QUILocale.get('quiqqer/system', 'zip'),
+                    labelCity      : QUILocale.get('quiqqer/system', 'city'),
+                    labelCountry   : QUILocale.get('quiqqer/system', 'country'),
+                    labelCompany   : QUILocale.get('quiqqer/system', 'company'),
+                    labelPhone     : QUILocale.get('quiqqer/system', 'tel'),
+                    labelMobile    : QUILocale.get('quiqqer/system', 'mobile'),
+                    labelFax       : QUILocale.get('quiqqer/system', 'fax')
                 })
             }).inject(this.$Input, 'after');
+
+            if (this.$Input.value.length) {
+                this.$Settings = JSON.decode(this.$Input.value);
+                this.$setSettings();
+            }
+
+            this.$Content.getElements('input').addEvent('change', function (event) {
+                var n      = event.target.get('name');
+                var c      = event.target.get('class');
+                var status = event.target.checked;
+
+                if (!(n in self.$Settings)) {
+                    self.$Settings[n] = {
+                        show    : false,
+                        required: false
+                    };
+                }
+
+                switch (c) {
+                    case 'quiqqer-frontendusers-settings-addressfields-show':
+                        self.$Settings[n].show = status;
+                        break;
+
+                    case 'quiqqer-frontendusers-settings-addressfields-required':
+                        self.$Settings[n].required = status;
+                        break;
+                }
+
+                if (!self.$Settings[n].show && self.$Settings[n].required) {
+                    if (c === 'quiqqer-frontendusers-settings-addressfields-required') {
+                        self.$Content.getElement(
+                            'input[name="' + n + '"].quiqqer-frontendusers-settings-addressfields-show'
+                        ).checked = true;
+
+                        self.$Settings[n].show = false;
+                    } else {
+                        self.$Content.getElement(
+                            'input[name="' + n + '"].quiqqer-frontendusers-settings-addressfields-required'
+                        ).checked = false;
+
+                        self.$Settings[n].required = false;
+                    }
+                }
+
+                self.$Input.value = JSON.encode(self.$Settings);
+            });
         },
 
         /**
          * Set settings to input
          */
         $setSettings: function () {
-            var entryElms     = this.$Input.getParent().getElements(
-                '.quiqqer-frontendusers-settings-addressfields-entry'
-            );
-            var RegistrarData = {};
+            for (var field in this.$Settings) {
+                if (!this.$Settings.hasOwnProperty(field)) {
+                    continue;
+                }
 
-            for (var i = 0, len = entryElms.length; i < len; i++) {
-                var EntryElm  = entryElms[i];
-                var registrar = btoa(EntryElm.get('data-registrar'));
+                this.$Content.getElement(
+                    'input[name="' + field + '"].quiqqer-frontendusers-settings-addressfields-show'
+                ).checked = this.$Settings[field].show;
 
-                RegistrarData[registrar] = QUIFormUtils.getFormData(
-                    EntryElm.getElement('form')
-                );
+                this.$Content.getElement(
+                    'input[name="' + field + '"].quiqqer-frontendusers-settings-addressfields-required'
+                ).checked = this.$Settings[field].required;
             }
-
-            this.$Input.value = JSON.encode(RegistrarData);
-        },
-
-        /**
-         * Get list of all registrars
-         *
-         * @return {Promise}
-         */
-        $getAddressFields: function () {
-            return new Promise(function (resolve, reject) {
-                QUIAjax.post('package_quiqqer_frontend-users_ajax_settings_getAddressFields', resolve, {
-                    'package': 'quiqqer/frontend-users',
-                    onError  : reject
-                });
-            });
         }
     });
 });
