ca2892642c0249fef25f11b68c43c730064fbfa5
Patrick Müller <p.mueller@pcsg.de>
2018-03-19 15:36:36 +0100

    feat: set and query confirm status of user standard email address #21

--- src/QUI/FrontendUsers/ActivationVerification.php
+++ src/QUI/FrontendUsers/ActivationVerification.php
@@ -39,8 +39,7 @@ class ActivationVerification extends AbstractVerification
             $User = QUI::getUsers()->get($userId);
             $User->activate(false, QUI::getUsers()->getSystemUser());
 
-            $User->setAttribute(Handler::USER_ATTR_EMAIL_VERIFIED, true);
-            $User->save(QUI::getUsers()->getSystemUser());
+            Utils::setUserEmailVerified($User);
         } catch (\Exception $Exception) {
             QUI\System\Log::addError(
                 self::class . ' :: onSuccess -> Could not find user #' . $userId
