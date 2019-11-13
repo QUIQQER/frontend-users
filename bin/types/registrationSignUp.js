8bdef74b8d6b0ff3fd0e4a2fcec78f0490c5eab9
michael.daniel <michael@pcsg.de>
2019-11-13 09:47:39 +0100

    fix: Do not display sing in links if fullscreen setting is not enabled.

--- bin/types/registrationSignUp.js
+++ bin/types/registrationSignUp.js
@@ -81,31 +81,35 @@ function signUpOnLoad() {
         }
     });
 
-    document.getElements('.registration-sign-in-links a').addEvent('click', function (event) {
-        event.stop();
+    var SignInLinks = document.getElements('.registration-sign-in-links');
 
-        var Target = event.target;
+    if (SignInLinks) {
+        SignInLinks.getElements('a').addEvent('click', function (event) {
+            event.stop();
 
-        if (Target.nodeName !== 'A') {
-            Target = Target.getParent('a');
-        }
+            var Target = event.target;
 
-        require([
-            'package/quiqqer/controls/bin/site/Window',
-            'Locale'
-        ], function (QUISiteWindow, QUILocale) {
-            var lg     = 'quiqqer/frontend-users',
-                sideId = Target.get('data-id');
+            if (Target.nodeName !== 'A') {
+                Target = Target.getParent('a');
+            }
 
-            new QUISiteWindow({
-                closeButtonText: QUILocale.get(lg, 'btn.close'),
-                showTitle      : true,
-                project        : QUIQQER_PROJECT.name,
-                lang           : QUIQQER_PROJECT.lang,
-                id             : sideId
-            }).open();
+            require([
+                'package/quiqqer/controls/bin/site/Window',
+                'Locale'
+            ], function (QUISiteWindow, QUILocale) {
+                var lg     = 'quiqqer/frontend-users',
+                    sideId = Target.get('data-id');
+
+                new QUISiteWindow({
+                    closeButtonText: QUILocale.get(lg, 'btn.close'),
+                    showTitle      : true,
+                    project        : QUIQQER_PROJECT.name,
+                    lang           : QUIQQER_PROJECT.lang,
+                    id             : sideId
+                }).open();
+            });
         });
-    });
+    }
 }
 
 if (typeof window.whenQuiLoaded !== 'undefined') {
