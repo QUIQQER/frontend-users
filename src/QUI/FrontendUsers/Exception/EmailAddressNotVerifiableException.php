11d1409d0fd81d2f8bfeec62c0df0a32d4b3a335
Henning <leutz@pcsg.de>
2026-07-18 12:24:24 +0200

    docs: explain exception code initialization

--- src/QUI/FrontendUsers/Exception/EmailAddressNotVerifiableException.php
+++ src/QUI/FrontendUsers/Exception/EmailAddressNotVerifiableException.php
@@ -9,6 +9,9 @@ class EmailAddressNotVerifiableException extends Exception
     private const DEFAULT_CODE = 50002;
 
     /**
+     * Exception::$code is untyped and must not be redeclared with a native type.
+     * The default code is therefore passed to the parent constructor.
+     *
      * @param string|array<int|string, mixed>|null $message
      * @param array<string, mixed> $context
      */
