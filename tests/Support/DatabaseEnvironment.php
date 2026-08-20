2870dea713fbc1dbb5dc6f51b061e32e0c638299
Henning <leutz@pcsg.de>
2026-08-20 21:04:53 +0200

    test: use configured database in GitLab CI

---
+++ tests/Support/DatabaseEnvironment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace QUI\FrontendUsers\Tests\Support;
+
+final class DatabaseEnvironment
+{
+    public const MODE_CI_DATABASE = 'ci-database';
+    public const MODE_SQLITE = 'sqlite';
+
+    /** @param array<string, string|false> $environment */
+    public static function determineMode(array $environment): string
+    {
+        return ($environment['GITLAB_CI'] ?? false) === 'true'
+            ? self::MODE_CI_DATABASE
+            : self::MODE_SQLITE;
+    }
+
+    public static function getMode(): string
+    {
+        $environment = getenv();
+
+        return self::determineMode(is_array($environment) ? $environment : []);
+    }
+
+    public static function usesCiDatabase(): bool
+    {
+        return self::getMode() === self::MODE_CI_DATABASE;
+    }
+}
